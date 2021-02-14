ac18a15867560e544ef5f22dc006cd8180162759
virtualphoton <zphoton0@gmail.com>
2021-02-15 02:33:24 +0300

    added exception handling for flask server abruption

--- simulation/server.py
+++ simulation/server.py
@@ -1,6 +1,7 @@
 import websockets
 import asyncio
 import time
+from websockets.exceptions import ConnectionClosedError
 
 FRAME_DELTA = 1 / 24
 
@@ -13,6 +14,10 @@ async def start_server_loop(camera):
         if (delta := last_call_time + FRAME_DELTA - time.time()) > 0:
             await asyncio.sleep(delta)
         last_call_time = time.time()
-        await websocket.send(camera.get_frame_bytes())
+        try:
+            await websocket.send(camera.get_frame_bytes())
+        except ConnectionClosedError:
+            # will be triggered when flask app is closed via ctrl-c and will prevent this from falling
+            pass
 
     await websockets.serve(hello, "localhost", 8765)
