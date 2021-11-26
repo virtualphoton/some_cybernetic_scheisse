3a3c09c24deaff9e899a5fd31c6ab43dd7bb923b
photon <zphoton0@gmail.com>
2021-11-26 11:05:26 +0300

    position detection update

--- automaton/break_the_pieces_A.py
+++
@@ -1,2 +0,0 @@
-def break_pieces(shape:str):
-    pluses = [i for i in range(len(shape)) if shape[i] == '+']
--- automaton/decorator.py
+++
@@ -1,6 +0,0 @@
-class change_detection:
-    def __init__(self, cls):
-        self.__obj = cls
-
-    def __ge
-
