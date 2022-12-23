b2fbf4a9d3231241195f36df171377d729e33711
virtual_photon <zphoton0@gmail.com>
2022-12-24 01:35:52 +0300

    constants

--- server/constants.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = b"\xaf\xd4a\xec\xdfJ'\xe5\xc5D#4D\xe4\xaa\x9f"
-CLIENT_ID = '543749207333-rscj6i8f4b6fvqj5bkepiso3rtt38dvk.apps.googleusercontent.com'
-CLIENT_SECRET = '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'
